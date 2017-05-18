2e6a6473080e1a516c73cb6b6c5a40cc41123038
adoconnection <adoconnection@gmail.com>
2017-05-18 18:37:08 +0300

    transmitter

--- RFMLib/Configuration/FskOok/RFM9XFskOokTransmitter.cs
+++ RFMLib/Configuration/FskOok/RFM9XFskOokTransmitter.cs
@@ -2,6 +2,11 @@
 {
     public class RFM9XFskOokTransmitter
     {
-        
+        private readonly TransceiverRegistry fifoBank;
+
+        public RFM9XFskOokTransmitter(ITransceiverSpiConnection connection)
+        {
+            this.fifoBank = new TransceiverRegistry(connection, 0x00);
+        }
     }
 }
